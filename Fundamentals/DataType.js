9d72fa00866a77cd1b9ebddc88e061b8f6fc2502
Rifqi31 <muttaqinrifqi31@gmail.com>
2019-03-19 19:20:19 +0700

    add example of data type

---
+++ Fundamentals/DataType.js
@@ -0,0 +1,24 @@
+// this is data type in JS
+"use strict";
+
+// numeber
+let thisnumber = 123345;
+// string
+let thisWord = "Fck this shit man!";
+//  boolean
+let valueright = true;
+let valuefalse = false;
+// null
+let death = null;
+
+console.log("this is example number : {$thisnumber}");
+console.log(thisWord);
+// test the boolean
+let handsome = valueright;
+if (handsome == valueright) {
+	console.log(valueright);
+} else {
+	console.log(valuefalse);
+}
+// test null
+console.log(typeof death);
