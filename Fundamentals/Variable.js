f772cc7b2121ac17dcf32afbf185c7a8049c2008
Rifqi31 <muttaqinrifqi31@gmail.com>
2019-03-19 19:20:52 +0700

    example of vaiable

---
+++ Fundamentals/Variable.js
@@ -0,0 +1,8 @@
+// this is var in modern way
+"use strict";
+
+let myname = "Rifqi Muttaqin";
+const personas = "Aroganism";
+
+console.log("his name is " + myname);
+console.log("and his personas is " + personas);
