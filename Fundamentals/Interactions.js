6a5d810ba0f7287e31f4b94779c79b3ae9352727
Rifqi31 <muttaqinrifqi31@gmail.com>
2019-03-20 14:11:07 +0700

    learn interactions for user in js

---
+++ Fundamentals/Interactions.js
@@ -0,0 +1,19 @@
+// modern way
+"use strict";
+
+// alert
+alert("Hello There I'm alert!!");
+
+// prompt
+let valuestring = prompt("how much learn for javascript ?", "free");
+alert(`of course is ${valuestring} from internet lol`);
+
+// confirm
+let musafir = confirm("are you musafir ?");
+alert(musafir); // True if OK is pressed by user
+
+if (musafir == true) {
+	alert("insyallah alhamdulillah")
+} else {
+	alert("god forgive me");
+}
