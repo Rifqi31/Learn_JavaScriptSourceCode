ca8419bd4818e1e26bac90cb87349e60c8f8bdc0
Rifqi31 <muttaqinrifqi31@gmail.com>
2019-03-20 14:11:47 +0700

    learn operators in js

---
+++ Fundamentals/Operators.js
@@ -0,0 +1,32 @@
+// modern way call
+"use strict";
+
+// operators in JS
+// String binary +
+let word1 = "Begone";
+let word2 = "Thot!!!";
+console.log(word1 + word2); //string type
+
+// Numberic unary +
+let num1 = 100;
+let num2 = 23;
+console.log(num1 / num2); // still number
+
+// assignment
+let foo = 20**2;			// this is exponation
+console.log(foo);
+
+// reminder
+console.log(num1 % num2);
+
+// exponation
+console.log(2**2);
+
+// increment and decrement
+let counter = 2;
+counter++;      // works the same as counter = counter + 1, but is shorter
+alert( counter ); // 3
+
+let decounter = 3;
+decounter--;
+alert(decounter);
